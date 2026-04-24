9d6ccf6830038ee0dba30bbf28217d85609434ee
Nbridge0 <nbridge0@users.noreply.github.com>
2026-04-24 10:41:11 +0200

    remove all junk untitled files

--- app/types/untitled1.py
+++
@@ -1,9 +0,0 @@
-class Questionnaire:
-    def __init__(self, strategy, hr, efficiency, financial, marketing, service, risk):
-        self.strategy = strategy
-        self.hr = hr
-        self.efficiency = efficiency
-        self.financial = financial
-        self.marketing = marketing
-        self.service = service
-        self.risk = risk
